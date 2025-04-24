30ed6404d8d95bb422997284892c319adb352d13
Kristin <kristindyulgeryan@abv.bg>
2025-04-24 13:28:11 +0300

    Add custom components and css modules.

---
+++ my-app/pages/[meetupId]/index.js
@@ -0,0 +1,15 @@
+import MeetupDetail from "@/components/meetups/Meetupdetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1024px-Stadtbild_M%C3%BCnchen.jpg"
+      alt="A First Meetup"
+      title="First Meetup"
+      address="Some street 5, Some city"
+      description="The is first meetup"
+    />
+  );
+}
+
+export default MeetupDetails;
