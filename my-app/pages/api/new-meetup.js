a5195ea2c18e09d843a6f9f68372ee7821d708fa
Kristin <kristindyulgeryan@abv.bg>
2025-05-04 22:19:45 +0300

    Install and configure MongoDB connection.

--- my-app/pages/api/new-meetup.js
+++ my-app/pages/api/new-meetup.js
@@ -1,7 +1,22 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
     const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://kristin:091888181@cluster0.zflq6r4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 export default handler;
